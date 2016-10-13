dc1735dbe32d28898b8b3f2371e45153af86da81
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2016-10-13 14:46:01 -0400

    Various updates including download throttling

--- patric_tools/amr.py
+++ patric_tools/amr.py
@@ -3,28 +3,14 @@
 
 """
 import numpy as np
+import os
 import pandas as pd
 
 from .config import PATRIC_FTP_AMR_METADATA_URL
-from .utils import download_file_from_url
+from .utils import download_file_from_url, url_extract_file_name
 
 
-def get_latest_metadata(outdir):
-    """
-    Downloads the latest antimicrobial resistance metadata
-
-    Parameters:
-    -----------
-    outdir: str
-        The path to the output directory
-
-    """
-    exception = download_file_from_url(PATRIC_FTP_AMR_METADATA_URL, outdir)
-    if exception != '':
-        raise RuntimeError("Failed to download the latest AMR metadata: %s" % exception)
-
-
-def genome_ids_by_antibiotic_and_species(amr_metadata_file, antibiotic, species=None, drop_intermediate=True):
+def get_amr_data_by_species_and_antibiotic(amr_metadata_file, antibiotic, species=None, drop_intermediate=True):
     """
     Returns the PATRIC identifiers of the genomes for which there is AMR metadata for some antibiotic
 
@@ -71,3 +57,53 @@ def genome_ids_by_antibiotic_and_species(amr_metadata_file, antibiotic, species=
     numeric_phenotypes[amr.resistant_phenotype.values == "Intermediate"] = 2
 
     return amr["genome_name"].values, amr["genome_id"].values, numeric_phenotypes
+
+
+def get_latest_metadata(outdir):
+    """
+    Downloads the latest antimicrobial resistance metadata
+
+    Parameters:
+    -----------
+    outdir: str
+        The path to the output directory
+
+    """
+    exception = download_file_from_url(PATRIC_FTP_AMR_METADATA_URL, outdir)
+    if exception != '':
+        raise RuntimeError("Failed to download the latest AMR metadata: %s" % exception)
+    return os.path.join(outdir, url_extract_file_name(PATRIC_FTP_AMR_METADATA_URL))
+
+
+def list_amr_datasets(amr_metadata_file, min_resistant=0, max_resistant=None, min_susceptible=0,
+                      max_susceptible=None, single_species=True):
+
+    if max_resistant is None:
+        max_resistant = np.infty
+    if max_susceptible is None:
+        max_susceptible = np.infty
+
+    amr = pd.read_table(amr_metadata_file, usecols=["genome_id", "genome_name", "antibiotic", "resistant_phenotype"],
+                        converters={'genome_id': str, 'genome_name': lambda x: " ".join(x.lower().split()[:2])})
+
+    dataset_species = []
+    dataset_antibiotics = []
+
+    if single_species:
+        amr = amr.groupby(['genome_name', 'antibiotic'])
+    else:
+        amr = amr.groupby('antibiotic')
+
+    for name, data in amr:
+        n_res = (data["resistant_phenotype"] == "Resistant").sum()
+        n_sus = (data["resistant_phenotype"] == "Susceptible").sum()
+
+        if min_resistant <= n_res <= max_resistant and min_susceptible <= n_sus <= max_susceptible:
+            if single_species:
+                dataset_antibiotics.append(name[1])
+                dataset_species.append([name[0]])
+            else:
+                dataset_antibiotics.append(name)
+                dataset_species.append(None)
+
+    return zip(dataset_species, dataset_antibiotics)
--- patric_tools/genomes.py
+++ patric_tools/genomes.py
@@ -8,20 +8,24 @@ try:
 except ImportError: # Python 3
     pass  # TODO: include urljoin
 
+from time import sleep
+
 from .config import PATRIC_FTP_GENOMES_FNA_URL
 from .utils import download_file_from_url
 
 
-def download_genome_contigs(patric_id, outdir="."):
+def download_genome_contigs(patric_id, outdir=".", throttle=False):
     """
     Downloads the contigs for a given genome
 
     Parameters:
     -----------
-    patric_id: str
-        The PATRIC identifier of the genome to download
     outdir: str
         The output directory in which to store the contig file
+    patric_id: str
+        The PATRIC identifier of the genome to download
+    throttle: bool
+        Whether or not to throttle the download by sleeping for a short period of time
 
     Returns:
     --------
@@ -29,6 +33,8 @@ def download_genome_contigs(patric_id, outdir="."):
         Empty string if no exception occurred, exception info otherwise
 
     """
+    if throttle:
+        sleep(2)  # Give that server a break!
     file_name = urljoin(PATRIC_FTP_GENOMES_FNA_URL, patric_id + ".fna")
     logging.debug("Downloading contigs for genome %s (%s)" % (patric_id, file_name))
     return download_file_from_url(file_name, outdir=outdir)
