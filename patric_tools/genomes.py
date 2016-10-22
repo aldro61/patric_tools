43e71b444bb22aced3b896469a1a877046ab5f34
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2016-10-21 22:35:17 -0400

    Downloading patric feature annotations

--- patric_tools/genomes.py
+++ patric_tools/genomes.py
@@ -10,7 +10,7 @@ except ImportError: # Python 3
 
 from time import sleep
 
-from .config import PATRIC_FTP_GENOMES_FNA_URL
+from .config import PATRIC_FTP_GENOMES_TAB_URL, PATRIC_FTP_GENOMES_FNA_URL
 from .utils import download_file_from_url
 
 
@@ -23,7 +23,7 @@ def download_genome_contigs(patric_id, outdir=".", throttle=False):
     outdir: str
         The output directory in which to store the contig file
     patric_id: str
-        The PATRIC identifier of the genome to download
+        The PATRIC identifier of the genome
     throttle: bool
         Whether or not to throttle the download by sleeping for a short period of time
 
@@ -37,4 +37,30 @@ def download_genome_contigs(patric_id, outdir=".", throttle=False):
         sleep(2)  # Give that server a break!
     file_name = urljoin(PATRIC_FTP_GENOMES_FNA_URL, patric_id + ".fna")
     logging.debug("Downloading contigs for genome %s (%s)" % (patric_id, file_name))
+    return download_file_from_url(file_name, outdir=outdir)
+
+
+def download_genome_patric_annotations(patric_id, outdir=".", throttle=False):
+    """
+    Downloads the PATRIC feature annotations for a given genome
+
+    Parameters:
+    -----------
+    outdir: str
+        The output directory in which to store the annotation file
+    patric_id: str
+        The PATRIC identifier of the genome
+    throttle: bool
+        Whether or not to throttle the download by sleeping for a short period of time
+
+    Returns:
+    --------
+    exception: str
+        Empty string if no exception occurred, exception info otherwise
+
+    """
+    if throttle:
+        sleep(2)  # Give that server a break!
+    file_name = urljoin(PATRIC_FTP_GENOMES_TAB_URL, patric_id + ".PATRIC.features.tab")
+    logging.debug("Downloading PATRIC annotations for genome %s (%s)" % (patric_id, file_name))
     return download_file_from_url(file_name, outdir=outdir)
