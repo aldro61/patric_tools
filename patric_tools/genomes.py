f75417d96ef5a12aea060bc91374db84ea1aa002
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2016-11-30 18:21:05 -0500

    Weird commit

--- patric_tools/genomes.py
+++ patric_tools/genomes.py
@@ -3,6 +3,7 @@
 
 """
 import logging
+import os
 try:
     from urlparse import urljoin
 except ImportError: # Python 3
@@ -10,8 +11,8 @@ except ImportError: # Python 3
 
 from time import sleep
 
-from .config import PATRIC_FTP_GENOMES_TAB_URL, PATRIC_FTP_GENOMES_FNA_URL
-from .utils import download_file_from_url
+from .config import PATRIC_FTP_GENOMES_FNA_URL, PATRIC_FTP_GENOMES_METADATA_URL, PATRIC_FTP_GENOMES_TAB_URL
+from .utils import download_file_from_url, url_extract_file_name
 
 
 def download_genome_contigs(patric_id, outdir=".", throttle=False):
@@ -64,3 +65,19 @@ def download_genome_patric_annotations(patric_id, outdir=".", throttle=False):
     file_name = urljoin(PATRIC_FTP_GENOMES_TAB_URL, patric_id + ".PATRIC.features.tab")
     logging.debug("Downloading PATRIC annotations for genome %s (%s)" % (patric_id, file_name))
     return download_file_from_url(file_name, outdir=outdir)
+
+
+def get_latest_metadata(outdir):
+    """
+    Downloads the latest genome metadata (not to be confused with AMR metadata)
+
+    Parameters:
+    -----------
+    outdir: str
+        The path to the output directory
+
+    """
+    exception = download_file_from_url(PATRIC_FTP_GENOMES_METADATA_URL, outdir)
+    if exception != '':
+        raise RuntimeError("Failed to download the latest AMR metadata: %s" % exception)
+    return os.path.join(outdir, url_extract_file_name(PATRIC_FTP_GENOMES_METADATA_URL))
